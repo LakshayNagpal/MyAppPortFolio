e73f7f76a0dfa5752af2de7bdbbb6cca1373a61f
Lakshay Nagpal <lakshay.nagpal00000@gmail.com>
2016-04-01 23:03:21 +0530

    ScrollView added

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,32 +15,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toast_display(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Spotify Streamer App", Toast.LENGTH_SHORT).show();
+        String toa = ((Button) view).getText().toString();
+        Toast.makeText(MainActivity.this, "This Button will launch my " + toa , Toast.LENGTH_SHORT).show();
         return;
     }
 
-    public void toast_display1(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Scores App", Toast.LENGTH_SHORT).show();
-        return;
-    }
-
-    public void toast_display2(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Library App", Toast.LENGTH_SHORT).show();
-        return;
-    }
-
-    public void toast_display3(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Build It Bigger App", Toast.LENGTH_SHORT).show();
-        return;
-    }
-
-    public void toast_display4(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Bacon Reader App", Toast.LENGTH_SHORT).show();
-        return;
-    }
-
-    public void toast_display5(View view){
-        Toast.makeText(MainActivity.this, "This Button will launch my Capstone Project App", Toast.LENGTH_SHORT).show();
-        return;
-    }
 }
